68d83239bb6700db7ae0e1adf389bcfc3a40cf6f
sahtiyan <135950003+sahtiyan@users.noreply.github.com>
2024-04-07 01:10:47 +0300

    Yeni argüman işleme

--- src/kayra.cpp
+++ src/kayra.cpp
@@ -1,51 +1,70 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "../include/Lexer.h"   // Lexer başlık dosyasını dahil et
-#include "../include/Parser.h"  // Parser başlık dosyasını dahil et
+#include <vector>
+#include <algorithm>
+#include "../include/Lexer.h"
+#include "../include/Parser.h"
 
 using namespace std;
 
+// Yardım metnini yazdıran fonksiyon
+void printHelp() {
+    cout << "Kullanım: kayra [SEÇENEKLER...] [DOSYA...]" << endl;
+    cout << endl;
+    cout << "SEÇENEKLER:" << endl;
+    cout << "   -r, --run     Kayra kaynak kod dosyalarını çalıştırmak için." << endl;
+    cout << "   -h, --help    Yardım menüsünü görüntülemek için." << endl;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "Kullanım: kayra run <dosya_adı.ky>" << endl;
+    if (argc < 2 || string(argv[1]) == "-h" || string(argv[1]) == "--help") {
+        printHelp();
+        return 0;
+    }
+
+    vector<string> args(argv + 1, argv + argc);
+    vector<string> files;
+
+    auto run_flag = find(args.begin(), args.end(), "-r");
+    if (run_flag == args.end()) run_flag = find(args.begin(), args.end(), "--run");
+
+    if (run_flag == args.end()) {
+        cout << "Geçersiz kullanım: En az bir dosya belirtilmeli." << endl;
         return 1;
+    } else {
+        for (auto it = next(run_flag); it != args.end(); ++it) {
+            if (it->at(0) == '-') {
+                cout << "Geçersiz bayrak: " << *it << endl;
+                return 1;
+            }
+            files.push_back(*it);
+        }
     }
 
-    string arg = argv[1];
-    if (arg != "run") {
-        cout << "Geçersiz komut. Sadece 'run' komutu desteklenmektedir." << endl;
-        return 1;
+    // Dosyaları işleme
+    for (const string& filename : files) {
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cout << "Dosya bulunamadı: " << filename << endl;
+            return 1;
+        }
+
+        string source;
+        string line;
+        while (getline(file, line)) {
+            source += line + "\n";
+        }
+        file.close();
+
+        // Lexer ile kaynak kodu tokenize et
+        Lexer lexer(source);
+        vector<Token> tokens = lexer.tokenize();
+
+        // Parser ile tokenleri analiz et ve işle
+        Parser parser(tokens);
+        parser.parse();
     }
-
-    if (argc < 3) {
-        cout << "Kullanım: kayra run <dosya_adı.ky>" << endl;
-        return 1;
-    }
-
-    string filename = argv[2];
-
-    // .ky dosyasını oku
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cout << "Dosya bulunamadı: " << filename << endl;
-        return 1;
-    }
-
-    string source;
-    string line;
-    while (getline(file, line)) {
-        source += line + "\n";
-    }
-    file.close();
-
-    // Lexer ile kaynak kodu tokenize et
-    Lexer lexer(source);
-    vector<Token> tokens = lexer.tokenize();
-
-    // Parser ile tokenleri analiz et ve işle
-    Parser parser(tokens);
-    parser.parse();
 
     return 0;
 }
